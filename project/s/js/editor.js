b58716338635c52bc1fbf039c322a1607774563d
Miguel Dorta <dortamiguel@gmail.com>
2015-03-21 01:51:17 +0000

    added ; to all the lines in edit.js

--- project/s/js/editor.js
+++ project/s/js/editor.js
@@ -1,19 +1,19 @@
 // Global editor variables
-var editor_section = document.getElementById('editor_section')
-var view_section = document.getElementById('view_section')
+var editor_section = document.getElementById('editor_section');
+var view_section = document.getElementById('view_section');
 
 // Form editor variables
-var id_title = document.getElementById('id_title')
-var id_body = document.getElementById('id_body')
-var id_tag = document.getElementById('id_tag')
-var id_pub_date = document.getElementById('id_pub_date')
+var id_title = document.getElementById('id_title');
+var id_body = document.getElementById('id_body');
+var id_tag = document.getElementById('id_tag');
+var id_pub_date = document.getElementById('id_pub_date');
 var editor_hidden = false;
 
 // Post view variables
-var postTitle = document.getElementById('post_title')
-var postBody = document.getElementById('post_content')
-var postTag = document.getElementById('post_tag')
-var postPubdate = document.getElementById('post_pub_date')
+var postTitle = document.getElementById('post_title');
+var postBody = document.getElementById('post_content');
+var postTag = document.getElementById('post_tag');
+var postPubdate = document.getElementById('post_pub_date');
 
 
 document.addEventListener('DOMContentLoaded', function() {
