e8f4c950d50e8f91bf88d4e451cc732b7bfaf3df
Miguel Dorta <dortamiguel@gmail.com>
2015-03-23 23:38:09 +0000

    Search changed to all

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -7,11 +7,11 @@ urlpatterns = patterns(
 	url(r'^feed/$', feed.BlogFeed(), name="feed"),
 
 	# Archivo
-	url(r'^search/$', views.ArchiveView.as_view(), name="search"),
+	url(r'^all/$', views.ArchiveView.as_view(), name="all"),
 	url(r'^author/(?P<author>\S+)/$', views.PostAuthorView.as_view(), name="author_view"),
 	url(r'^tag/(?P<tag>\S+)/$', views.PostTagView.as_view(), name="tag_view"),
 	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',  views.PostDateView.as_view(), name="date_view"),
-
+	
 	# Blog
 	url(r'^$', views.FrontView.as_view(), name="front"),
 	url(r'^(?P<pk>[-\w]+)/(?P<slug>.*)/$',  views.PostView.as_view(), name="post_view"),
