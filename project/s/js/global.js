f01b1393b83b96453ca14f3a28e21593d99c79ca
Miguel Dorta <dortamiguel@gmail.com>
2015-04-13 18:05:27 +0100

    Better minimum width for show_mode 1

--- project/s/js/global.js
+++ project/s/js/global.js
@@ -45,9 +45,9 @@ info_nav_link.addEventListener('click', function() {
 
 
 function set_show_mode() {
-	if (window_width > 950) { show_mode = 3; }
-	else if ( window_width > 600 && window_width < 950) { show_mode = 2; }
-	else if ( window_width < 600) { show_mode = 1; }
+	if (window_width >= 950) { show_mode = 3; }
+	else if ( window_width > 480 && window_width < 950) { show_mode = 2; }
+	else if ( window_width <= 480) { show_mode = 1; }
 
 	set_panel_visibility();
 }
