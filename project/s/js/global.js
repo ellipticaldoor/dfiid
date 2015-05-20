72ae176049b0b532b4ff3bd3870fae797a1ba42a
Miguel Dorta <dortamiguel@gmail.com>
2015-05-20 16:41:16 +0100

    show mode_2 removed

--- project/s/js/global.js
+++ project/s/js/global.js
@@ -8,8 +8,7 @@ var mid_nav_link = document.getElementById('mid_nav_link');
 var info_nav_link = document.getElementById('info_nav_link');
 
 // UI logic related
-var show_mode = 3;
-var mid_or_info = true; // true is show mid
+var show_mode = 2;
 var main_panel = 2; // 1 == link, 2 == mid, 2 == info
 
 var window_width = window.innerWidth 
@@ -58,76 +57,49 @@ link_nav_link.addEventListener('click', function() {
 
 mid_nav_link.addEventListener('click', function() {
 	main_panel = 2;
-	mid_or_info = true;
 	set_panel_visibility();
 });
 
 info_nav_link.addEventListener('click', function() {
 	main_panel = 3;
-	mid_or_info = false;
 	set_panel_visibility();
 });
 
 
 function set_show_mode() {
-	if (window_width >= 800) { show_mode = 3; }
-	else if ( window_width > 650 && window_width < 800) { show_mode = 2; }
-	else if ( window_width <= 650) { show_mode = 1; }
+	if (window_width > 950) { show_mode = 2; }
+	else { show_mode = 1; }
 
 	set_panel_visibility();
 }
 
 
 function set_selected() {
-	if (show_mode == 2 ) {
-		if (mid_or_info == true) {
-			mid_nav_link.classList.add('selected');
-			info_nav_link.classList.remove('selected');
-		}
-		else if (mid_or_info == false) {
-			mid_nav_link.classList.remove('selected');
-			info_nav_link.classList.add('selected');
-		}
+	if (main_panel == 1) {
+		link_nav_link.classList.add('selected');
+		mid_nav_link.classList.remove('selected');
+		info_nav_link.classList.remove('selected');
 	}
-	else if (show_mode == 1 ) {
-		if (main_panel == 1) {
-			link_nav_link.classList.add('selected');
-			mid_nav_link.classList.remove('selected');
-			info_nav_link.classList.remove('selected');
-		}
-		else if (main_panel == 2) {
-			link_nav_link.classList.remove('selected');
-			mid_nav_link.classList.add('selected');
-			info_nav_link.classList.remove('selected');
-		}
-		else if (main_panel == 3) {
-			link_nav_link.classList.remove('selected');
-			mid_nav_link.classList.remove('selected');
-			info_nav_link.classList.add('selected');
-		}
+	else if (main_panel == 2) {
+		link_nav_link.classList.remove('selected');
+		mid_nav_link.classList.add('selected');
+		info_nav_link.classList.remove('selected');
+	}
+	else if (main_panel == 3) {
+		link_nav_link.classList.remove('selected');
+		mid_nav_link.classList.remove('selected');
+		info_nav_link.classList.add('selected');
 	}
 }
 
 
 function set_panel_visibility() {
-	if (show_mode == 3) {
+	if (show_mode == 2) {
 		link_panel.style.display = 'block';
 		mid_panel.style.display = 'block';
 		info_panel.style.display = 'block';
 	}
-	else if (show_mode == 2 ) {
-		link_panel.style.display = 'block';
-		if (mid_or_info == true) {
-			mid_panel.style.display = 'block';
-			info_panel.style.display = 'none';
-		}
-		else if (mid_or_info == false) {
-			mid_panel.style.display = 'none';
-			info_panel.style.display = 'block';
-		}
-		set_selected();
-	}
-	else if(show_mode == 1) {
+	else {
 		if (main_panel == 1) {
 			link_panel.style.display = 'block';
 			mid_panel.style.display = 'none';
