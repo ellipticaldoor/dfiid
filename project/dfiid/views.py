897790001186230cb3e78f5a3d99bb74b02c0a13
Miguel Dorta <dortamiguel@gmail.com>
2015-06-06 16:57:25 +0100

    tab_select links changed to li elements

--- project/dfiid/views.py
+++ project/dfiid/views.py
@@ -8,14 +8,12 @@ class About(TemplateView):
 
 
 def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
 
 
 def handler500(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+	response.status_code = 500
+	return response
