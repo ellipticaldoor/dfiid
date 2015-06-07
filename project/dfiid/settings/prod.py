5d13502894588c28888cd04617688fc493536d2a
Miguel Dorta <dortamiguel@gmail.com>
2015-06-07 13:16:45 +0100

    memcached added

--- project/dfiid/settings/prod.py
+++ project/dfiid/settings/prod.py
@@ -3,3 +3,10 @@ from dfiid.settings.base import *
 DEBUG = False
 
 COMPRESS_ENABLED = True
+
+CACHES = {
+	'default': {
+		'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+		'LOCATION': '127.0.0.1:11211',
+	}
+}
